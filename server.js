9d1efef53b98aa942a1932636c2b84ab0607862e
Paul Dkn <pauldkn.code@gmail.com>
2020-12-09 12:56:23 +0100

    add: ApolloClient, Launches_Query && display launches

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 const { graphqlHTTP } = require("express-graphql");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 const schema = require("./schema");
 
 const app = express();
 
-app.use(bodyParser.json());
+app.use(cors());
+// app.use(bodyParser.json());
 
 app.use(
   "/graphql",
